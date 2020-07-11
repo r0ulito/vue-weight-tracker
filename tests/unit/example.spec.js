1eac3f08f33c3e7420313e66c65f2a648898220e
= <roulit77@gmail.com>
2020-07-11 02:55:41 +0200

    Changed the sample test to match changes made in files

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,16 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('App.vue', () => {
+  it('renders msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+    const wrapper = shallowMount(App, {
+      data() {
+        return {
+          msg,
+        };
+      },
     });
     expect(wrapper.text()).to.include(msg);
   });
